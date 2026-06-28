7b61e38ac74dccec9cb900d02850159edca9d908
David <david@cornejod-lk4xny.lan>
2026-06-28 17:39:58 +0200

    v0.3.6: iconos sin padding, llenan el cuadrado completo

---
+++ pwa-assets.config.js
@@ -0,0 +1,8 @@
+export default {
+  preset: {
+    transparent: { sizes: [64, 192, 512], padding: 0 },
+    maskable:    { sizes: [512],          padding: 0 },
+    apple:       { sizes: [180],          padding: 0 },
+  },
+  images: ['public/icon.svg'],
+}
